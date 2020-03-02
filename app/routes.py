f8ac1be12fd89988d47f1966a5aa59b2193ba057
arch-jw <18408129+arch-jw@users.noreply.github.com>
2020-03-02 08:25:16 +0000

    working, basic user profiles

--- app/routes.py
+++ app/routes.py
@@ -56,3 +56,14 @@ def register():
         flash('Thank you for registering!')
         return redirect(url_for('login'))
     return render_template('registration.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test Post #1'},
+        {'author': user, 'body': 'Test Post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
